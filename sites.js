7f5408f123b2c9578eb5e5d1cd6b6b9cfb496d13
David A. Lindahl <davidalindahl@gmail.com>
2020-05-18 08:42:01 -0700

    Update sites.js
    Adding my wee lil site

--- sites.js
+++ sites.js
@@ -2,4 +2,5 @@ let sites = [
     { name: "Jack McDade", url: 'https://jackmcdade.com'},
     { name: "Paul Jarvis", url: 'https://pjrvs.com'},
     { name: "Jesse Leite", url: 'https://jesseleite.com'},
+    { name: "David A. Lindahl", url: 'https://davidalindahl.com'},
 ]
