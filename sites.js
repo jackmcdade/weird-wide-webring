a83b69678fb9c50886a829b9c86987a044828a78
Jack McDade <jack@jackmcdade.com>
2020-05-19 10:56:25 -0400

    Added becauses

--- sites.js
+++ sites.js
@@ -1,16 +1,73 @@
 let sites = [
-    { name: "Jack McDade", url: 'https://jackmcdade.com'},
-    { name: "Paul Jarvis", url: 'https://pjrvs.com'},
-    { name: "Mighty Fine Copy", url: 'https://www.mightyfinecopy.com/'},
-    { name: "Luigi Battaglioli", url: 'https://theluigi.com'},
-    { name: "Ross Wintle", url: 'https://rosswintle.uk/'},
-    { name: "David A. Lindahl", url: 'https://davidalindahl.com'},
-    { name: "Miguel Piedrafita", url: 'https://miguelpiedrafita.com'},
-    { name: "Teevio", url: 'https://teev.io'},
-    { name: "Matt Lantz", url: 'https://mattlantz.ca'},
-    { name: "Jamie Howard", url: 'https://jamiehoward.co'},
-    { name: "Jesse Leite", url: 'https://jesseleite.com'},
-    { name: "David Llop", url: 'https://davidllop.com'},
-    { name: "Aaron Francis", url: 'https://aaronfrancis.com/'},
-    { name: "Colin Dismuke", url: 'https://www.colindismuke.com'},
+    {
+        name: "Jack McDade",
+        url: 'https://jackmcdade.com',
+        because: "You'll know."
+    },
+    {
+        name: "Mighty Fine Copy",
+        url: 'https://www.mightyfinecopy.com/',
+        because: "Colors! Words! Ideas!"
+    },
+    {
+        name: "Paul Jarvis",
+        url: 'https://pjrvs.com',
+        because: "Articles with a powerfully unique perspective"
+    },
+    {
+        name: "Luigi Battaglioli",
+        url: 'https://theluigi.com',
+        because: "Writes about weirdness."
+    },
+    {
+        name: "Ross Wintle",
+        url: 'https://rosswintle.uk/',
+        because: "Unique design complete with fake terminal!"
+
+    },
+    {
+        name: "David A. Lindahl",
+        url: 'https://davidalindahl.com',
+        because: "Rad colors and unique side projects."
+    },
+    {
+        name: "Miguel Piedrafita",
+        url: 'https://miguelpiedrafita.com',
+        because: "Prolific teenager programmers are inspiring!"
+    },
+    {
+        name: "Teevio",
+        url: 'https://teev.io',
+        because: "Lots of unique content!"
+    },
+    {
+        name: "Matt Lantz",
+        url: 'https://mattlantz.ca',
+        because: "Says controversial things about tech stuff."
+    },
+    {
+        name: "Jamie Howard",
+        url: 'https://jamiehoward.co',
+        because: "Beautiful illustrations."
+    },
+    {
+        name: "David Llop",
+        url: 'https://davidllop.com',
+        because: "That font! Those colors!"
+    },
+    {
+        name: "Jesse Leite",
+        url: 'https://jesseleite.com',
+        because: "Master of puns."
+    },
+    {
+        name: "Colin Dismuke",
+        url: 'https://www.colindismuke.com',
+        because: "Lots of unique content."
+    },
+    {
+        name: "Aaron Francis",
+        url: 'https://aaronfrancis.com/',
+        because: "Has a wierd list of things that work when broken."
+    },
 ]
