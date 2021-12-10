148662001533347a1ff128ed8f460255a3c498ac
Jack McDade <jack@jackmcdade.com>
2021-12-10 14:37:35 -0500

    Fix JS

--- sites.js
+++ sites.js
@@ -188,6 +188,6 @@ let sites = [
     {
         name: "fLaMEdFury",
         url: "https://flamedfury.com",
-        because: ""My personal homepage, full of stuff that means everything to me, and nothing to you. fLaMEdFury draws inspiration from a web that existed 20 years ago!"
+        because: "My personal homepage, full of stuff that means everything to me, and nothing to you. fLaMEdFury draws inspiration from a web that existed 20 years ago!"
     }
 ]
